26972a589ae9cbb947c05a86d805f53d6753158b
torupa <xpt77@outlook.com>
2023-08-24 22:43:13 +0900

    save png file

--- models/mnist_utils.py
+++ models/mnist_utils.py
@@ -1,15 +1,18 @@
 import cv2
 import torchvision
 import numpy as np
+from os import path
 
 dataset = torchvision.datasets.MNIST('./data/',
                                      transform=torchvision.transforms.ToTensor(),
                                      download=True)
 
-for i in range(0, 100):
-    img = dataset[i][0] * 255
-    npimg = np.transpose(img.numpy(), (1, 2, 0))
-    print(npimg.shape)
-    outfile = 'img{i:04d}.png'.format(i=i)
-    cv2.imwrite(outfile, npimg)
-#Image.fromarray(npimg, mode=mode)
+def generate_png_file(dataset, outdir:str='.'):
+    for i in range(0, 100):
+        img = dataset[i][0] * 255
+        npimg = np.transpose(img.numpy(), (1, 2, 0))
+        outfile = path.join(outdir, 'img{i:04d}.png'.format(i=i))
+        cv2.imwrite(outfile, npimg)
+        print(outfile)
+
+generate_png_file(dataset)
